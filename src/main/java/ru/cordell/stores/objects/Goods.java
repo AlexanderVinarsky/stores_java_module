e82f71b5ee75d6158d7b9d61f26b8cab2437fcc5
j1sk1ss <fotikolay222@gmail.com>
2023-08-18 19:10:17 +0500

    objects

---
+++ src/main/java/ru/cordell/stores/objects/Goods.java
@@ -0,0 +1,8 @@
+package ru.cordell.stores.objects;
+
+public class Goods {
+    public String Code;
+    public String Id;
+    public String Name;
+    public float Price;
+}
---
+++ src/main/java/ru/cordell/stores/objects/Store.java
@@ -0,0 +1,11 @@
+package ru.cordell.stores.objects;
+
+import java.util.List;
+
+public class Store {
+    public List<Goods> Storage;
+    public String Name;
+    public String CompanyName;
+    public float xCoordinate;
+    public float yCoordinate;
+}
